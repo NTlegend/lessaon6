6a1c2a70e714a30b6c8b2036f6133db0ded53dbc
shushik81 <shushik81@gmail.com>
2018-12-05 19:03:35 +0200

    create static method load and loadAll

--- index.js
+++ index.js
@@ -2,23 +2,58 @@ require('dotenv').config();
 const mysql = require('mysql');
 const util = require('util');
 
+const Car = require('./car');
+const User = require('./user');
+
 global.db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 global.db.query = util.promisify(global.db.query);
 
-
-// Открыть с БД и вывести в консоль сузествующего пользователя с машинами
+// Открыть с БД и вывести в консоль суествующего пользователя с машинами
+console.log(User.load(1));
+console.log(User.loadAll());
 
 // Создать нового пользователя
+const user = new User();
+user.first_name = 'Alexey';
+user.last_name = 'Bobrov';
+user.age = '42';
+user.gender = 'M';
 
 // Изменить имя пользователю
+const user_id = user.save();
+user.first_name = 'Dmitriy';
+user.save(user_id);
+
+// Добавить пользователю новую машину
+const car = new Car();
+car.user_id = user_id;
+car.model = 'volga';
+car.year = '1998';
+const car_id = car.save();
 
 // Удалить пользователя
 
-// Добавить пользователю новую машину
+
+// Закрываем соединение с базой
+global.db.end();
+
+/*
+
+Нужно реадизовать паттерн ActiveRecord, а именно в файле model.js нужно написать методы:
+load - получение экземпляра одной строки с БД по первичному ключу
+loadAll - получение массива экземпляров класса
+save - сохранение в БД (если PK не задан - то создание, иначе - обновление)
+delete - удаление с БД
+
+эти методы должны быть универсальны, чтобы один и тот же код корректно работал для обоих моделей  User и Car.
+
+Полученый код нужно зхакомитить и запушить в git репозиторий и присылайте мне ссылочку. желательно коммиты делать поэтапно.
+
+*/
--- model.js
+++ model.js
@@ -1,5 +1,70 @@
 class Model {
-  // do magic
+
+    static load(pk) {
+        global.db.query('SELECT * FROM ?? WHERE id = ?', [this.table(), pk], function (error, results) {
+            if (error) {
+                console.log(error.message);
+                throw error;
+            }
+            console.log(results);
+            return results;
+        });
+    }
+
+    static loadAll() {
+        global.db.query('SELECT * FROM ??', this.table(), function (error, results) {
+            if (error) {
+                console.log(error.message);
+                throw error;
+            }
+            console.log(results);
+            return results;
+        });
+    }
+
+    save(pk = null) {
+        const table = this.constructor.table();
+        let inserts = {};
+        if (table === 'users') {
+            inserts = {
+                first_name: this.first_name,
+                last_name: this.last_name,
+                age: this.age,
+                gender: this.gender
+            };
+        }
+        if (table === 'cars') {
+            inserts = {
+                user_id: pk,
+                model: this.model,
+                year: this.year
+            };
+        }
+
+        if (pk === null) {
+            const sql = 'INSERT INTO ?? SET ?';
+            global.db.query(sql, [table, inserts], function (error, results) {
+                if (error) {
+                    console.log(error.message);
+                    throw error;
+                }
+                console.log(results.insertId);
+                this.pk = results.insertId;
+                return results.insertId;
+            });
+        } else {
+            const sql = 'UPDATE ?? SET ? WHERE id = ?';
+            global.db.query(sql, [table, inserts, pk], function (error, results) {
+                if (error) {
+                    console.log(error.message);
+                    throw error;
+                }
+                console.log('changed user id' + pk);
+                return pk;
+            });
+        }
+        global.db.end();
+    }
 }
 
 module.exports = Model;
